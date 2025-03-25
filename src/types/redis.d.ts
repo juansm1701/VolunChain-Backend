3039b10b977535e2e1fc11139d0bee7e5fa86fa4
clintjeff2 <clintjeff2@gmail.com>
2025-03-25 06:17:50 +0100

    Redis type file

---
+++ src/types/redis.d.ts
@@ -0,0 +1,17 @@
+
+declare module "redis" {
+  import { EventEmitter } from "events";
+
+  export interface RedisClientOptions {
+    url?: string;
+  }
+
+  export interface RedisClientType extends EventEmitter {
+    connect(): Promise<void>;
+    disconnect(): Promise<void>;
+    ping(): Promise<string>;
+    on(event: string, listener: (...args: any[]) => void): this;
+  }
+
+  export function createClient(options?: RedisClientOptions): RedisClientType;
+}
