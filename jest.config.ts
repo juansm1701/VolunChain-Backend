c79cdbc441285c1db6a471c7a3460e7d4cdb19eb
clintjeff2 <clintjeff2@gmail.com>
2025-03-25 07:57:25 +0100

    commented previous config and added new one, new one works fine

--- jest.config.ts
+++ jest.config.ts
@@ -1,15 +1,38 @@
-import { config } from "dotenv";
+// import { config } from "dotenv";
 
-config({ path: ".env.test" });
+// config({ path: ".env.test" });
 
+// module.exports = {
+//   preset: "ts-jest",
+//   testEnvironment: "node",
+//   transform: {
+//     "^.+\\.tsx?$": [
+//       "ts-jest",
+//       {
+//         tsconfig: "./tsconfig.json",
+//       },
+//     ],
+//   },
+//   testMatch: ["**/tests/**/*.test.ts", "**/__tests__/**/*.test.ts"],
+//   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+// };
+
+// jest.config.js;
+/** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    transform: {
-        '^.+\\.tsx?$': ['ts-jest', {
-            tsconfig: './tsconfig.json'
-        }]
-    },
-    testMatch: ['**/tests/**/*.test.ts', '**/__tests__/**/*.test.ts'],
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
+  preset: "ts-jest",
+  testEnvironment: "node",
+  roots: ["<rootDir>/tests"],
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+  },
+  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  collectCoverageFrom: [
+    "src/**/*.{js,ts}",
+    "!**/node_modules/**",
+    "!**/tests/**",
+  ],
+  coverageDirectory: "coverage",
+  testTimeout: 30000, // Increased timeout for async tests
 };
