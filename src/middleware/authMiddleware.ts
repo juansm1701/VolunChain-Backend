a54961d80aa3f526f594bdffa4f56aa2c416ed35
clintjeff2 <clintjeff2@gmail.com>
2025-04-26 04:23:48 +0100

    Changed user repository to use PrimaRepository, user repository was faulty

--- src/middleware/authMiddleware.ts
+++ src/middleware/authMiddleware.ts
@@ -1,44 +1,49 @@
-import { Request, Response, NextFunction } from 'express';
-import jwt from 'jsonwebtoken';
-import { AppDataSource } from '../config/data-source'; 
-import { UserRepository } from '../repository/user.repository';
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+// import { AppDataSource } from "../config/data-source";
+// import { UserRepository } from "../repository/user.repository";
+import { PrismaUserRepository } from "../modules/user/repositories/PrismaUserRepository";
 
 interface AuthenticatedRequest extends Request {
   user?: {
-    id: any;
+    id: string;
     role: string;
     isVerified: boolean;
   };
 }
 
-const SECRET_KEY = process.env.JWT_SECRET || 'defaultSecret';
+const SECRET_KEY = process.env.JWT_SECRET || "defaultSecret";
 
-
-const userRepository = new UserRepository(AppDataSource);
+const userRepository = new PrismaUserRepository();
 
 const authMiddleware = async (
   req: Request,
   res: Response,
   next: NextFunction
 ): Promise<void> => {
-  const token = req.headers.authorization?.split(' ')[1];
+  const token = req.headers.authorization?.split(" ")[1];
 
   if (!token) {
-    res.status(401).json({ message: 'No token provided' });
+    res.status(401).json({ message: "No token provided" });
     return;
   }
 
   try {
-    const decoded = jwt.verify(token, SECRET_KEY) as { id: string; role: string };
-    const user = await userRepository.findById(decoded.id);
+    const decoded = jwt.verify(token, SECRET_KEY) as {
+      id: string;
+      role: string;
+    };
+    const user = await userRepository.findById(`${decoded.id}`);
 
     if (!user) {
-      res.status(401).json({ message: 'User not found' });
+      res.status(401).json({ message: "User not found" });
       return;
     }
 
     if (!user.isVerified) {
-      res.status(403).json({ message: 'Email not verified. Please verify your email to proceed.' });
+      res.status(403).json({
+        message: "Email not verified. Please verify your email to proceed.",
+      });
       return;
     }
 
@@ -51,12 +56,12 @@ const authMiddleware = async (
     (req as AuthenticatedRequest).user = {
       id: user.id,
       role: decoded.role,
-      isVerified: user.isVerified
+      isVerified: user.isVerified,
     };
 
     next();
   } catch (error) {
-    res.status(401).json({ message: 'Invalid token' });
+    res.status(401).json({ message: error?.message || "Invalid token" });
   }
 };
 
