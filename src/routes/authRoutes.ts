c2e461e8ef3fdc67c461433f9803b2ebe3af7d59
clintjeff2 <clintjeff2@gmail.com>
2025-04-26 04:15:29 +0100

    Added register route

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,15 +1,18 @@
-import { Router } from 'express';
-import AuthController from '../controllers/Auth.controller';
-import authMiddleware from '../middleware/authMiddleware';
+import { Router } from "express";
+import AuthController from "../controllers/Auth.controller";
+import authMiddleware from "../middleware/authMiddleware";
 
 const router = Router();
 
-router.post('/login', AuthController.login);
-router.get('/protected', authMiddleware, AuthController.protectedRoute);
+router.post("/login", AuthController.login);
+router.post("/register", AuthController.register);
+router.get("/protected", authMiddleware, AuthController.protectedRoute);
 
-
-router.post('/send-verification-email', AuthController.resendVerificationEmail);
-router.get('/verify-email', AuthController.verifyEmail);
-router.post('/resend-verification-email', AuthController.resendVerificationEmail);
+router.post("/send-verification-email", AuthController.resendVerificationEmail);
+router.get("/verify-email", AuthController.verifyEmail);
+router.post(
+  "/resend-verification-email",
+  AuthController.resendVerificationEmail
+);
 
 export default router;
