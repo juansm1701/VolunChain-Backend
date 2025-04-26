374900c277547e067252bec05307dadc0beeb24b
clintjeff2 <clintjeff2@gmail.com>
2025-04-26 04:14:59 +0100

    Edited volunteer route for best practice too

--- src/routes/VolunteerRoutes.ts
+++ src/routes/VolunteerRoutes.ts
@@ -4,13 +4,13 @@ import VolunteerController from "../controllers/VolunteerController";
 const router = Router();
 const volunteerController = new VolunteerController();
 
-router.post("/volunteers", async (req, res) =>
+router.post("/", async (req, res) =>
   volunteerController.createVolunteer(req, res)
 );
-router.get("/volunteers/:id", async (req, res) =>
+router.get("/:id", async (req, res) =>
   volunteerController.getVolunteerById(req, res)
 );
-router.get("/projects/:projectId/volunteers", async (req, res) =>
+router.get("/projects/:projectId", async (req, res) =>
   volunteerController.getVolunteersByProjectId(req, res)
 );
 
